c3938218256c5843ef6a476daa2b19a9678ab584
James Smith <jamesplease2@gmail.com>
2018-04-18 09:29:25 -0700

    Remove redux-resource-plugins

--- src/state/gists/reducer.js
+++ src/state/gists/reducer.js
@@ -1,15 +1,3 @@
 import { resourceReducer } from 'redux-resource';
-import { httpStatusCodes } from 'redux-resource-plugins';
 
-export default resourceReducer('gists', {
-  plugins: [
-    // This plugin gives us access to the HTTP Status Codes from our requests. The primary
-    // use case for this is differentiating different failed requests. For instance,
-    // did the request fail because the user was unauthorized, or because the resource
-    // was not found? For more on the HTTP Status Codes plugin, see the documentation at:
-    // https://redux-resource.js.org/docs/extras/http-status-codes-plugin.html
-    // If you're using, say, gRPC, then you would want to write a similar plugin that
-    // handles the gRPC status codes.
-    httpStatusCodes
-  ]
-});
+export default resourceReducer('gists');
